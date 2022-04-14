064a982de88a8d9889e990bdd7b9b69be93a8425
Hector Flores <hr.flowers@hotmail.com>
2022-04-14 13:57:16 -0500

    Adding dependencies

--- src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderServiceApplication.java
@@ -3,8 +3,11 @@ package com.javatechie.aws.cicd.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Comparator;
@@ -13,16 +16,22 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/")
 public class OrderServiceApplication {
 
     @Autowired
     private OrderDao orderDao;
 
-    @GetMapping
+    @GetMapping("/orders")
     public List<Order> fetchOrders() {
         return orderDao.getOrders().stream().
                 sorted(Comparator.comparing(Order::getPrice)).collect(Collectors.toList());
+    }
+
+    @GetMapping("/health")
+    public @ResponseBody
+    ResponseEntity<?> getHealth(){
+        return  new ResponseEntity<>("REST end point:" + HttpStatus.OK, HttpStatus.OK);
     }
 
     public static void main(String[] args) {
